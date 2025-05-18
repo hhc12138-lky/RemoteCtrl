4e00c5d9a278c9cdee4478c36065bfc6fc48f397
hhc <2446166228@qq.com>
2025-05-18 15:31:21 +0800

    1 尝试解决多线程发送冲突 ps：发现新的问题，对于文件/文件夹的信息获取还有问题

--- RemoteClient/ClientController.cpp
+++ RemoteClient/ClientController.cpp
@@ -60,7 +60,9 @@ int CClientController::SendCommandPacket(int nCmd, bool bAutoClose, BYTE* pData,
 {
 	CClientSocket* pClient = CClientSocket::getInstance();
 	if (pClient->InitSocket() == false) return false;
-	pClient->Send(CPacket(nCmd, pData, nLength));
+	HANDLE hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+	//TODO：不应该直接发送，而是投入队列
+	pClient->Send(CPacket(nCmd, pData, nLength, hEvent));
 	int cmd = DealCommand();
 	TRACE("ack:%d\r\n", cmd);
 	if (bAutoClose)
--- RemoteClient/ClientSocket.h
+++ RemoteClient/ClientSocket.h
@@ -3,6 +3,8 @@
 #include "framework.h"
 #include <string>
 #include <vector>
+#include <list>
+#include <map>
 
 #pragma pack(push)
 #pragma pack(1)
@@ -10,7 +12,7 @@ class CPacket
 {
 public:
 	CPacket() :sHead(0), nLength(0), sCmd(0), sSum(0) {}
-	CPacket(WORD nCmd, const BYTE* pData, size_t nSize) {
+	CPacket(WORD nCmd, const BYTE* pData, size_t nSize,HANDLE hEvent) {
 		sHead = 0xFEFF;
 		nLength = nSize + 4;
 		sCmd = nCmd;
@@ -26,6 +28,7 @@ public:
 		{
 			sSum += BYTE(strData[j]) & 0xFF;
 		}
+		this->hEvent = hEvent;
 	}
 	CPacket(const CPacket& pack) {
 		sHead = pack.sHead;
@@ -33,8 +36,9 @@ public:
 		sCmd = pack.sCmd;
 		strData = pack.strData;
 		sSum = pack.sSum;
+		hEvent = pack.hEvent;
 	}
-	CPacket(const BYTE* pData, size_t& nSize) {
+	CPacket(const BYTE* pData, size_t& nSize):hEvent(INVALID_HANDLE_VALUE){
 		size_t i = 0;
 		for (; i < nSize; i++) {
 			if (*(WORD*)(pData + i) == 0xFEFF) {
@@ -79,6 +83,7 @@ public:
 			sCmd = pack.sCmd;
 			strData = pack.strData;
 			sSum = pack.sSum;
+			hEvent = pack.hEvent;
 		}
 		return *this;
 	}
@@ -103,6 +108,7 @@ public:
 	std::string strData;//包数据
 	WORD sSum;//和校验
 	//std::string strOut;//整个包的数据
+	HANDLE hEvent;//事件句柄
 };
 #pragma pack(pop)
 
@@ -227,6 +233,8 @@ public:
 		m_nPort = nPort;
 	}
 private:
+	std::list<CPacket> m_lstSend; 
+	std::map<HANDLE, std::list<CPacket>> m_mapAck; //用于存储不同指令应带回来的不连续的包；对随机取值无要求，因此使用双向列表
 	int m_nIP;//地址
 	int m_nPort;//端口
 	std::vector<char> m_buffer;
@@ -248,8 +256,13 @@ private:
 	}
 	~CClientSocket() {
 		closesocket(m_sock);
+		m_sock = INVALID_SOCKET;
 		WSACleanup();
 	}
+
+	static void threadEntry(void* arg);
+	void threadFunc();
+
 	BOOL InitSockEnv() {
 		WSADATA data;
 		if (WSAStartup(MAKEWORD(1, 1), &data) != 0) {
